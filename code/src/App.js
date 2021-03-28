88fc79a651cdb09f0a3998e2ec7f56b23f400745
Jani <jani@muikku.org>
2021-03-28 17:51:01 +0200

    empty state and animations

--- code/src/App.js
+++ code/src/App.js
@@ -7,20 +7,19 @@ const allData = musicData.albums.items
 
 export const App = () => {
   const [data, setData] = useState(allData);
-  if (data === undefined || data.length === 0) {
-    console.log('Empty state')
-  }
   return (
     <>
       <Header toChild={data} sendToParent={setData} />
       <section className="albums">
-        {data.map((item) => {
-          return (
-            <div className="album" key={item.id}>
-              <Album key={item.id} item={item} />
-            </div>
-          )
-        })}
+        {(!data.length)
+          ? <div className="empty-state"><h1>Couldn&apos;t find any music..</h1></div>
+          : data.map((item) => {
+            return (
+              <div className="album" key={item.id}>
+                <Album key={item.id} item={item} />
+              </div>
+            )
+          })}
       </section>
     </>
   )
